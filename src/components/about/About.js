9a3e4cc3787d296eeaa2646542f3c463b90bbc68
Tishhshaw <Tish.shaw@yahoo.com>
2022-01-26 13:22:52 -0500

    add scaffolded out About.js

---
+++ src/components/about/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function About(props) {
+	return (
+		<div className='About-container'>
+			<h1 className='About__title'>About</h1>
+			<div className='About__text'>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti,
+				voluptatem! Sint amet placeat debitis adipisci pariatur dicta minus
+				omnis saepe itaque doloribus sequi cumque, earum non aspernatur
+				obcaecati illo nihil deleniti hic corporis odio ea excepturi inventore
+				eligendi. Nihil pariatur blanditiis ratione porro placeat, ipsum minus
+				expedita minima quos quis inventore esse, aliquid, fuga harum ducimus
+				tempora quia labore error reiciendis neque? Maiores est architecto sequi
+				similique tenetur doloremque repudiandae enim, saepe, autem rem soluta?
+				Magnam sit quaerat doloribus voluptatem nulla veniam, nemo error
+				voluptates facere cupiditate inventore illum explicabo autem soluta
+				adipisci voluptate odit?
+			</div>
+		</div>
+	);
+}
+
+export default About;
