29a4e4e1ca3c3238a228bfbeb0719907543ee04b
Tishhshaw <Tish.shaw@yahoo.com>
2022-01-27 11:03:00 -0500

    add changes to CreatePosts component

--- src/components/plants/CreatePosts.js
+++ src/components/plants/CreatePosts.js
@@ -4,13 +4,13 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+/*  imported mui modal refernce: https://mui.com/components/modal/#main-content -> discussed with Zoe on 1/27 and was approved */
 
 function CreatePosts(props) {
-	const [open, setOpen] = useState(false);
 	const [modal, setModal] = useState(false);
 
-	const handleOpen = () => setOpen(true);
-	const handleClose = () => setOpen(false);
+	const handleOpen = () => setModal(true);
+	const handleClose = () => setModal(false);
 	const style = {
 		position: 'absolute',
 		top: '50%',
@@ -20,7 +20,7 @@ function CreatePosts(props) {
 		height: 400,
 		bgcolor: 'background.paper',
 		border: '2px solid #000',
-		boxShadow: 24,
+		boxShadow: '10px 5px 5px red',
 		p: 4,
 	};
 
@@ -34,7 +34,7 @@ function CreatePosts(props) {
 			<Button onClick={handleOpen}>
 				<AddCircleIcon />
 			</Button>
-			<Modal open={open} onClose={handleClose}>
+			<Modal open={modal} onClose={handleClose}>
 				<Box sx={style}>
 					<Typography id='modal-modal-title' variant='h6' component='h2'>
 						Create an Angel
