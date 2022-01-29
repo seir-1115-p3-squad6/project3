74bd6cfb1302f4fae00fe5364a99f3979681887c
katlynhutson <kmhutson5@gmail.com>
2022-01-28 18:13:12 -0600

    Style the cards for a mobile screen

--- src/components/plants/Plants.js
+++ src/components/plants/Plants.js
@@ -18,39 +18,49 @@ function Plants(props) {
 			.catch(console.error);
 	}, []);
 	return (
-		<section className='container'>
+		<section>
 			<CreatePosts />
-			{plants.map((plant) =>
-				!plant.favorite ? (
-					<div className='card' key={plant._id}>
-						<Link to={`${plant._id}`}>
-							{/* <div className='card'> */}
-							{/* <div className='card-image'> */}
-							<img src={plant.image} alt={plant.name} className='image' />
-							{/* </div>
+			<div className='card-container'>
+				{plants.map((plant) =>
+					!plant.favorite ? (
+						<div className='card' key={plant._id}>
+							<Link to={`${plant._id}`}>
+								{/* <div className='card'> */}
+								{/* <div className='card-image'> */}
+								<img
+									src={plant.image}
+									alt={plant.name}
+									className='card-image'
+								/>
+								{/* </div>
 								<div className='card-title'> */}
-							<h3>{plant.name}</h3>
-							{/* </div> */}
-							{/* </div> */}
-						</Link>
-						<FavoriteIcon className='anti-heart'></FavoriteIcon>
-					</div>
-				) : (
-					<div key={plant._id} className='card'>
-						<Link to={`${plant._id}`}>
-							{/* <div className='card'> */}
-							{/* <div className='card-image'> */}
-							<img src={plant.image} alt={plant.name} className='image' />
-							{/* </div> */}
-							{/* <div className='card-title'> */}
-							<h3>{plant.name}</h3>
-							{/* </div> */}
-							{/* </div> */}
-						</Link>
-						<FavoriteIcon className='heart'></FavoriteIcon>
-					</div>
-				)
-			)}
+								<h3 className='card-name'>{plant.name}</h3>
+								{/* </div> */}
+								{/* </div> */}
+							</Link>
+							<FavoriteIcon className='anti-heart'></FavoriteIcon>
+						</div>
+					) : (
+						<div key={plant._id} className='card'>
+							<Link to={`${plant._id}`}>
+								{/* <div className='card'> */}
+								{/* <div className='card-image'> */}
+								<img
+									src={plant.image}
+									alt={plant.name}
+									className='card-image'
+								/>
+								{/* </div> */}
+								{/* <div className='card-title'> */}
+								<h3 className='card-name'>{plant.name}</h3>
+								{/* </div> */}
+								{/* </div> */}
+							</Link>
+							<FavoriteIcon className='heart'></FavoriteIcon>
+						</div>
+					)
+				)}
+			</div>
 		</section>
 	);
 }
